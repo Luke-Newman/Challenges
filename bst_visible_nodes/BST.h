533d593005cfa5487f6c222337ebf6cbf2991860
Luke Newman <lukenewman@live.com.au>
2018-06-09 19:15:25 +1000

    added max_height function to BST

--- bst_visible_nodes/BST.h
+++ bst_visible_nodes/BST.h
@@ -5,229 +5,245 @@
 #ifndef TREE_BST_H
 #define TREE_BST_H
 
+#include <iomanip>
 #include <iostream>
 #include <memory>
-#include <iomanip>
+#include <stack>
 #include <vector>
 
 template <class T>
-class BST
-{
-    struct NODE;
-    typedef std::unique_ptr<NODE> spNODE;
-private:
-    struct NODE
-    {
-        T data;
-        spNODE left;
-        spNODE right;
-        explicit NODE(const T &value):data(value),left(nullptr),right(nullptr){}
-    };
+class BST {
+  struct NODE;
+  typedef std::unique_ptr<NODE> spNODE;
 
-    spNODE root;
-    void _print(const spNODE &node, int level) const;
-    void _inOrder(const spNODE &p) const;
-    void _preOrder(const spNODE &p) const;
-    void _postOrder(const spNODE &p) const;
-    void _markVisibleNodesPredecessor(const spNODE &p, std::vector<int> &nodes,
-                                      int highest) const;
-    void _markVisibleNodesParent(const spNODE &p, std::vector<int> &nodes,
-                                      int parent) const;
+ private:
+  struct NODE {
+    T data;
+    spNODE left;
+    spNODE right;
+    explicit NODE(const T &value)
+        : data(value), left(nullptr), right(nullptr) {}
+  };
 
-public:
-    BST() { root = nullptr; }
-    void insert(const T & ref);
-    void print_BST()const;
-    bool tree_empty() const { return root == nullptr; }
-    void print_inOrder();
-    void print_preOrder();
-    void print_postOrder();
-    void getVisibleNodesPredecessor() const;
-    void getVisibleNodesParent() const;
+  spNODE root;
+  void _print(const spNODE &node, int level) const;
+  void _inOrder(const spNODE &p) const;
+  void _preOrder(const spNODE &p) const;
+  void _postOrder(const spNODE &p) const;
+  void _markVisibleNodesPredecessor(const spNODE &p, std::vector<int> &nodes,
+                                    int highest) const;
+  void _markVisibleNodesParent(const spNODE &p, std::vector<int> &nodes,
+                               int parent) const;
 
-
+ public:
+  BST() { root = nullptr; }
+  void insert(const T &ref);
+  void print_BST() const;
+  bool tree_empty() const { return root == nullptr; }
+  void print_inOrder();
+  void print_preOrder();
+  void print_postOrder();
+  void getVisibleNodesPredecessor() const;
+  void getVisibleNodesParent() const;
+  void getMaxHeight();
 };
 
-template<class T>
-void BST<T>::insert(const T & ref) {
-    std::unique_ptr<NODE> node(new NODE(ref));
+template <class T>
+void BST<T>::insert(const T &ref) {
+  std::unique_ptr<NODE> node(new NODE(ref));
 
-    if (root == nullptr)
-        root = std::move(node);
-    else {
-        NODE* tmp = root.get();
-        NODE* prev = root.get();
+  if (root == nullptr)
+    root = std::move(node);
+  else {
+    NODE *tmp = root.get();
+    NODE *prev = root.get();
 
-        /* move down tree from root until we reach empty child */
-        while (tmp != nullptr)
-        {
-            prev = tmp;
-            if (tmp->data < ref)
-                tmp = tmp->right.get();
-            else
-                tmp = tmp->left.get();
-        }
-
-        if (prev->data < ref)
-            prev->right = std::move(node);
-        else
-            prev->left = std::move(node);
+    /* move down tree from root until we reach empty child */
+    while (tmp != nullptr) {
+      prev = tmp;
+      if (tmp->data < ref)
+        tmp = tmp->right.get();
+      else
+        tmp = tmp->left.get();
     }
+
+    if (prev->data < ref)
+      prev->right = std::move(node);
+    else
+      prev->left = std::move(node);
+  }
 }
 
-template<class T>
-void BST<T>::print_BST()const
-{
-    _print(root, 0);
+template <class T>
+void BST<T>::print_BST() const {
+  _print(root, 0);
 }
 
-template<class T>
-void BST<T>::_print(const spNODE &node, int level) const
-{
-    int i;
+template <class T>
+void BST<T>::_print(const spNODE &node, int level) const {
+  int i;
 
-    /* Very basic print out of tree */
+  /* Very basic print out of tree */
 
-    if (node) {
-        _print(node->right, level + 1);
+  if (node) {
+    _print(node->right, level + 1);
 
-        std::cout << "Level: " << std::setw(3) << level;
+    std::cout << "Level: " << std::setw(3) << level;
 
-        for (i = 0; i <= level; i++)
-            std::cout << "....";
+    for (i = 0; i <= level; i++) std::cout << "....";
 
-        std::cout << std::setw(3) << node->data << std::setw(3) << '\n';
+    std::cout << std::setw(3) << node->data << std::setw(3) << '\n';
 
-        _print(node->left, level + 1);
-    }
+    _print(node->left, level + 1);
+  }
 }
 
-template<class T>
-void BST<T>::print_inOrder()
-{
-    if (tree_empty()) return;
-    _inOrder(root);
+template <class T>
+void BST<T>::print_inOrder() {
+  if (tree_empty()) return;
+  _inOrder(root);
 }
 
-template<class T>
-void BST<T>::_inOrder(const spNODE &p) const
-{
-    /*
-        1. Traverse the left subtree, i.e., call Inorder(left-subtree)
-        2. Visit the root.
-        3. Traverse the right subtree, i.e., call Inorder(right-subtree)
-    */
+template <class T>
+void BST<T>::_inOrder(const spNODE &p) const {
+  /*
+      1. Traverse the left subtree, i.e., call Inorder(left-subtree)
+      2. Visit the root.
+      3. Traverse the right subtree, i.e., call Inorder(right-subtree)
+  */
 
-    if(p->left) _inOrder(p->left);
-    std::cout << " " << p->data << " ";
-    if(p->right) _inOrder(p->right);
+  if (p->left) _inOrder(p->left);
+  std::cout << " " << p->data << " ";
+  if (p->right) _inOrder(p->right);
 }
 
-template<class T>
+template <class T>
 void BST<T>::print_preOrder() {
-    if (tree_empty()) return;
-    _preOrder(root);
+  if (tree_empty()) return;
+  _preOrder(root);
 }
 
-template<class T>
-void BST<T>::_preOrder(const spNODE &p) const
-{
-    /*
-        1. Visit the root.
-        2. Traverse the left subtree, i.e., call preOrder(left-subtree)
-        3. Traverse the right subtree, i.e., call _preOrder(right-subtree)
-     */
+template <class T>
+void BST<T>::_preOrder(const spNODE &p) const {
+  /*
+      1. Visit the root.
+      2. Traverse the left subtree, i.e., call preOrder(left-subtree)
+      3. Traverse the right subtree, i.e., call _preOrder(right-subtree)
+   */
 
-    std::cout << " " << p->data << " ";
-    if (p->left) _preOrder(p->left);
-    if (p->right) _preOrder(p->right);
+  std::cout << " " << p->data << " ";
+  if (p->left) _preOrder(p->left);
+  if (p->right) _preOrder(p->right);
 }
 
-template<class T>
+template <class T>
 void BST<T>::print_postOrder() {
-    if (tree_empty()) return;
-    _postOrder(root);
+  if (tree_empty()) return;
+  _postOrder(root);
 }
 
-template<class T>
-void BST<T>::_postOrder(const spNODE &p) const
-{
-    /*
-        1. Traverse the left subtree, i.e., call postOrder(left-subtree)
-        2. Traverse the right subtree, i.e., call _postOrder(right-subtree)
-        3. Visit the root.
-     */
+template <class T>
+void BST<T>::_postOrder(const spNODE &p) const {
+  /*
+      1. Traverse the left subtree, i.e., call postOrder(left-subtree)
+      2. Traverse the right subtree, i.e., call _postOrder(right-subtree)
+      3. Visit the root.
+   */
 
-    if (p->left) _postOrder(p->left);
-    if (p->right) _postOrder(p->right);
-    std::cout << " " << p->data << " ";
+  if (p->left) _postOrder(p->left);
+  if (p->right) _postOrder(p->right);
+  std::cout << " " << p->data << " ";
 }
 
-template<class T>
-void BST<T>::getVisibleNodesPredecessor() const
-{
-    /* Parent is referering to the immediate parent of child node */
-    std::vector<int> nodes_list;
+template <class T>
+void BST<T>::getVisibleNodesPredecessor() const {
+  /* Parent is referering to the immediate parent of child node */
+  std::vector<int> nodes_list;
 
-    if (tree_empty()) return;
+  if (tree_empty()) return;
 
-    std::cout << "\n\nList of visible nodes higher than the parent\nwhere "
-            "parent "
-                 "is referring to the predecessor with the highest value:\n";
+  std::cout << "\n\nList of visible nodes higher than the parent\nwhere "
+               "parent "
+               "is referring to the predecessor with the highest value:\n";
 
-    _markVisibleNodesPredecessor(root, nodes_list, root->data);
+  _markVisibleNodesPredecessor(root, nodes_list, root->data);
 
-    for (int i : nodes_list)
-        std::cout << i << " ";
+  for (int i : nodes_list) std::cout << i << " ";
 
-    printf("\n");
+  printf("\n");
 }
 
-template<class T>
-void BST<T>::_markVisibleNodesPredecessor(const BST::spNODE &p, std::vector<int>
-&nodes, int highest) const
-{
-    if (p->data > highest)
-    {
-        highest = p->data;
-        nodes.push_back(highest);
+template <class T>
+void BST<T>::_markVisibleNodesPredecessor(const BST::spNODE &p,
+                                          std::vector<int> &nodes,
+                                          int highest) const {
+  if (p->data > highest) {
+    highest = p->data;
+    nodes.push_back(highest);
+  }
+
+  /* Could get away with only checking right for my tree, but this will also
+     will work if the tree prioritises highest to left*/
+  if (p->left) _markVisibleNodesPredecessor(p->left, nodes, highest);
+  if (p->right) _markVisibleNodesPredecessor(p->right, nodes, highest);
+}
+
+template <class T>
+void BST<T>::getVisibleNodesParent() const {
+  /* Parent is referering to the immediate parent of child node */
+  std::vector<int> nodes_list;
+
+  if (tree_empty()) return;
+
+  std::cout << "\nList of visible nodes higher than parent,\nwhere parent "
+               "is referring to the immediate parent of two child nodes:\n";
+
+  _markVisibleNodesParent(root, nodes_list, root->data);
+
+  for (int i : nodes_list) std::cout << i << " ";
+
+  printf("\n");
+}
+
+template <class T>
+void BST<T>::_markVisibleNodesParent(const BST::spNODE &p,
+                                     std::vector<int> &nodes,
+                                     int parent) const {
+  if (p->data > parent) nodes.push_back(p->data);
+
+  if (p->left) _markVisibleNodesParent(p->left, nodes, p->data);
+  if (p->right) _markVisibleNodesParent(p->right, nodes, p->data);
+}
+
+template <class T>
+void BST<T>::getMaxHeight() {
+  /* longest will either be left most or right most branch */
+  std::stack<NODE *> s;
+  int max_depth = 0, current_depth = 0;
+
+  s.push(this->root.get());
+
+  while (!s.empty()) {
+    NODE *current = s.top();
+    s.pop();
+
+    /* if both leaves are null we have reached the end of a branch */
+    if (!current->left && !current->right) {
+      if (current_depth > max_depth) {
+        max_depth = current_depth;
+        current_depth = 0;
+      }
     }
 
-    /* Could get away with only checking right for my tree, but this will also
-       will work if the tree prioritises highest to left*/
-    if (p->left) _markVisibleNodesPredecessor(p->left, nodes, highest);
-    if (p->right) _markVisibleNodesPredecessor(p->right, nodes, highest);
+    if (current->left) {
+      s.push(current->left.get());
+    }
+    if (current->right) {
+      s.push(current->right.get());
+    }
+    current_depth++;
+  }
+  std::cout << "\nMax height: " << max_depth - 1 << '\n';
 }
 
-template<class T>
-void BST<T>::getVisibleNodesParent() const
-{
-    /* Parent is referering to the immediate parent of child node */
-    std::vector<int> nodes_list;
-
-    if (tree_empty()) return;
-
-    std::cout << "\nList of visible nodes higher than parent,\nwhere parent "
-            "is referring to the immediate parent of two child nodes:\n";
-
-    _markVisibleNodesParent(root, nodes_list, root->data);
-
-    for (int i : nodes_list)
-        std::cout << i << " ";
-
-    printf("\n");
-}
-
-template<class T>
-void
-BST<T>::_markVisibleNodesParent(const BST::spNODE &p, std::vector<int> &nodes,
-                                int parent) const
-{
-    if (p->data > parent)
-        nodes.push_back(p->data);
-
-    if (p->left) _markVisibleNodesParent(p->left, nodes, p->data);
-    if (p->right) _markVisibleNodesParent(p->right, nodes, p->data);
-}
-
-#endif //TREE_BST_H
+#endif  // TREE_BST_H
--- bst_visible_nodes/main.cpp
+++ bst_visible_nodes/main.cpp
@@ -1,27 +1,30 @@
 #include "BST.h"
 
-int main()
-{
-    BST<int> bst;
-    bst.insert(4);
-    bst.insert(3);
-    bst.insert(5);
-    bst.insert(1);
-    bst.insert(8);
-    bst.insert(7);
-    bst.insert(3);
-    bst.insert(9);
-    bst.insert(5);
-    bst.insert(4);
-    bst.print_BST();
+int main() {
+  BST<int> bst;
+  bst.insert(4);
+  bst.insert(3);
+  bst.insert(5);
+  bst.insert(1);
+  bst.insert(8);
+  bst.insert(7);
+  bst.insert(3);
+  bst.insert(9);
+  bst.insert(5);
+  bst.insert(4);
+  bst.print_BST();
 
-    printf("\nIn Order:\n");
-    bst.print_inOrder();
-    printf("\nPre Order:\n");
-    bst.print_preOrder();
-    printf("\nPost Order:\n");
-    bst.print_postOrder();
+  printf("\nIn Order:\n");
+  bst.print_inOrder();
+  printf("\nPre Order:\n");
+  bst.print_preOrder();
+  printf("\nPost Order:\n");
+  bst.print_postOrder();
 
-    bst.getVisibleNodesPredecessor();
-    bst.getVisibleNodesParent();
+  bst.getVisibleNodesPredecessor();
+  bst.getVisibleNodesParent();
+
+  bst.getMaxHeight();
+
+  return 0;
 }
